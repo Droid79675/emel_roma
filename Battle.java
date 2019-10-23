2fb3abcaed3f8efd3ac776d14db9417332107867
Роман Емельянов <roman9900@inbox.ru>
2019-10-23 23:53:51 +0400

    Сделал супер доисторическое подобие игры,захотел чтобы каждое сражение сохранялось, но пишет exception, но в основе программа должна работать

---
+++ Battle.java
@@ -0,0 +1,11 @@
+public class Battle {
+    public String info;
+    public String getInfo() {
+        return info;
+    }
+    public Battle (String info){
+        this.info = info;
+    }
+
+
+}
